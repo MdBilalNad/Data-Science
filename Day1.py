7026fecb8b0f9aa18d3bd69bf7e53cd98b488038
Mohammed Bilal Nadeem <mdbilalnad117@gmail.com>
2026-06-02 14:49:00 +0530

    Add Hello World and basic function examples

---
+++ Day1.py
@@ -0,0 +1,18 @@
+# DAY 1: Hello World and Functions
+
+
+print("Hello, World!")
+
+def total(x , y):
+    return x + y
+
+print("Please input the values")
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+print("The total is: ", total(x, y)) 
+ 
+def Bilal(x,y):
+    return x * y
+for i in range(1, 5):
+    print("The total is: ", Bilal(i, i+1))
+
